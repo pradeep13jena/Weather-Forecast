4f1f4bdcfba5d15d309a460b4aef579bb45f04b7
pradeep13jena <goldrushatjenas@gmail.com>
2024-08-08 09:55:38 +0530

    Getting started

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./main/index.html"],
+  theme: {
+    fontFamily: {
+      'khitan': ["Noto Serif Khitan Small Script", 'serif'],
+      'gupter': ["Gupter", "serif"]
+  },
+    extend: {},
+  },
+  plugins: [],
+}
+
