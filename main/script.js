70c89cab3bffd30077316a84969ff032d8080272
pradeep13jena <goldrushatjenas@gmail.com>
2024-08-12 19:35:07 +0530

    added few condition for valid input and everything, major feature added is as soon as the site will get reloaded the data will be updated to san francisco and fresh data will be displayed. As soon as new input or city is entered no matter how many time you will reload it it'll stay on the same location/city

--- main/script.js
+++ main/script.js
@@ -1,21 +1,17 @@
-// For Search bar
+// For Search bar and search button
 const searchBar = document.querySelector('#search-bar');
 const suggestions = document.querySelector('#suggestion-box');
 const useLocation = document.getElementById('use-location');
 const onlyForUseCurrent = document.getElementById('onlyForUseCurrent')
-
-// For button
 const btn = document.querySelector('button');
 
-// For Date 
+// This is a regular expression which checks the value if it's a string or not
+const regex = /^[A-Za-z\s]+$/
 
+// Finding the date  
 const now = document.getElementById('time')
 
-// const now = new Date()
-// const date = now.toISOString().split('T')[0];
-
-
-// Finding the value of current weather data to display
+// Finding the values from object model to display current weather data
 const degreeValue = document.getElementById('degreeValue')
 const cityLocattion = document.getElementById('cityLocattion')
 const dateAndTime = document.getElementById('dateAndTime')
@@ -25,129 +21,73 @@ const humiToday = document.getElementById('humiToday')
 const windToday = document.getElementById('windToday')
 const valueText = document.getElementById('valueText')
 
-// Finding the value of Current + 1 weather data to display
+// Finding the values from object model to display 1st day weather data
 const imageWeatherOne = document.getElementById('imageWeatherOne')
 const tempTodayOne = document.getElementById('tempTodayOne')
 const humiTodayOne = document.getElementById('humiTodayOne')
 const windTodayOne = document.getElementById('windTodayOne')
 const dateOne = document.getElementById('dateForecasrOne')
 
-// Finding the value of Current + 2 weather data to display
+// Finding the values from object model to display 2nd day weather data
 const imageWeatherTwo = document.getElementById('imageWeatherTwo')
 const tempTodayTwo = document.getElementById('tempTodayTwo')
 const humiTodayTwo = document.getElementById('humiTodayTwo')
 const windTodayTwo = document.getElementById('windTodayTwo')
 const dateTwo = document.getElementById('dateForecasrTwo')
 
-// Finding the value of Current + 3 weather data to display
-
+// Finding the values from object model to display 3rd day weather data
 const imageWeatherThree = document.getElementById('imageWeatherThree')
 const tempTodayThree = document.getElementById('tempTodayThree')
 const humiTodayThree = document.getElementById('humiTodayThree')
 const windTodayThree = document.getElementById('windTodayThree')
 const dateThree = document.getElementById('dateForecasrThree')
 
-// Finding the value of Current + 4 weather data to display
-
+// Finding the values from object model to display 4th day weather data
 const imageWeatherFour = document.getElementById('imageWeatherFour')
 const tempTodayFour = document.getElementById('tempTodayFour')
 const humiTodayFour = document.getElementById('humiTodayFour')
 const windTodayFour = document.getElementById('windTodayFour')
 const dateFour = document.getElementById('dateForecasrFour')
 
-// Finding the value of Current + 5 weather data to display
-
+// Finding the values from object model to display 5th day weather data
 const imageWeatherFive = document.getElementById('imageWeatherFive')
 const tempTodayFive = document.getElementById('tempTodayFive')
 const humiTodayFive = document.getElementById('humiTodayFive')
 const windTodayFive = document.getElementById('windTodayFive')
 const dateFive = document.getElementById('dateForecasrFive')
 
-// Created a async function which takes city value and then feed the UI
-async function feedUI(citie){
+// Function to do stuff when the page is reloaded
+function restoreElement(){
+    // Pull value from session storage to display the search box
+    savedItem = sessionStorage.getItem('saveIT')
+    savedCItyInSess = sessionStorage.getItem('savedCity')
 
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), 5000);
+    // IF value is null (as there is nothing in there) then display the "use current Location"
+    if (savedItem === null) {
+        suggestions.innerHTML = onlyForUseCurrent.innerHTML
+    } else { // if it's not null display the stored data
+        suggestions.innerHTML = savedItem
+    }
 
-    try{
-        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c3d1cab6249d4373bc3131136240608&q=${citie}&days=5`, {
-            signal: controller.signal, // Pass the AbortController signal
-        });
-    
-        clearTimeout(timeoutId);
-    
-        // Check if the response is ok
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        } else{
-            searchBar.value = ''
-        }
+    // IF savedCity is null (as there is nothing in there) in session storage then display 'Mumbai' as default city
+    if(savedCItyInSess === null){
+        feedUI('San Francisco')
+    } else { //If there is some value in session storage for savedCIty then display that value
+        feedUI(savedCItyInSess)
+    }
 
-        const data = await response.json()
-        
-        now.innerHTML = `${data.location.localtime}`
-        degreeValue.innerHTML = `${Math.trunc(data.current.temp_c)}&degc`
-        cityLocattion.innerHTML = `${data.location.name.charAt(0).toUpperCase() + data.location.name.slice(1)}`
-        dateAndTime.innerHTML = `(${data.location.region}, ${data.location.country})`
-        imageWeather.src = `${data.current.condition.icon}`
-        preciToday.innerHTML = `${data.current.precip_mm}%`
-        humiToday.innerHTML = `${data.current.humidity}%`
-        windToday.innerHTML = `${data.current.wind_kph}kph`
-        valueText.innerHTML = `${data.current.condition.text}`
-
-        // Updating 1st day forecast
-
-        dateOne.innerHTML = data.forecast.forecastday[0].date
-        imageWeatherOne.src = data.forecast.forecastday[0].day.condition.icon
-        tempTodayOne.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[0].day.avgtemp_c)}&degc`
-        windTodayOne.innerHTML = `Wind - ${data.forecast.forecastday[0].day.maxwind_kph} kph`
-        humiTodayOne.innerHTML = `Humi - ${data.forecast.forecastday[0].day.avghumidity}%`
-        
-        
-        dateTwo.innerHTML = data.forecast.forecastday[1].date
-        imageWeatherTwo.src = data.forecast.forecastday[1].day.condition.icon
-        tempTodayTwo.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[1].day.avgtemp_c)}&degc`
-        windTodayTwo.innerHTML = `Wind - ${data.forecast.forecastday[1].day.maxwind_kph} kph`
-        humiTodayTwo.innerHTML = `Humi - ${data.forecast.forecastday[1].day.avghumidity}%`
-
-        dateThree.innerHTML = data.forecast.forecastday[2].date
-        imageWeatherThree.src = data.forecast.forecastday[2].day.condition.icon
-        tempTodayThree.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[2].day.avgtemp_c)}&degc`
-        windTodayThree.innerHTML = `Wind - ${data.forecast.forecastday[2].day.maxwind_kph} kph`
-        humiTodayThree.innerHTML = `Humi - ${data.forecast.forecastday[2].day.avghumidity}%`
-
-        dateFour.innerHTML = data.forecast.forecastday[3].date
-        imageWeatherFour.src = data.forecast.forecastday[3].day.condition.icon
-        tempTodayFour.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[3].day.avgtemp_c)}&degc`
-        windTodayFour.innerHTML = `Wind - ${data.forecast.forecastday[3].day.maxwind_kph} kph`
-        humiTodayFour.innerHTML = `Humi - ${data.forecast.forecastday[3].day.avghumidity}%`
-
-        dateFive.innerHTML = data.forecast.forecastday[4].date
-        imageWeatherFive.src = data.forecast.forecastday[4].day.condition.icon
-        tempTodayFive.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[4].day.avgtemp_c)}&degc`
-        windTodayFive.innerHTML = `Wind - ${data.forecast.forecastday[4].day.maxwind_kph} kph`
-        humiTodayFive.innerHTML = `Humi - ${data.forecast.forecastday[4].day.avghumidity}%`
-
-        
-    } catch (error) {
-        // Handle fetch-related errors and timeouts
-        if (error.name === "AbortError") {
-          alert("Request timed out. Please try again.")
-        } else if (error.message.startsWith("HTTP error!")) {
-           alert(`No Such city as "${cityName}", Enter Correct name of city `)
-        } else {
-          alert(`Network error: ${error.message}`)
-        }
-      }
 }
 
-// For the suggestion box, as soon as the suggestion box is focused you can see all the option of the cities
+// Calling the function everytime the window browser is reloaded
+window.onload = restoreElement // this onload is at the top as it'll make it quicker to reload the page
+
+// For the suggestion box, as soon as the search bar is focused you can see all the option of the cities that have been saved
 searchBar.addEventListener('focus', () => {
     suggestions.classList.remove('suggestion');
     suggestions.classList.add('active');
 });
 
-// For the suggestion box, as soon as the suggestion box is out of focused you can see all the option of the cities
+// For the suggestion box, as soon as the search bar is out of focused you can see all the option of the cities get disappear
 searchBar.addEventListener('blur', () => {
     setTimeout(() => {
         suggestions.classList.remove('active');
@@ -155,61 +95,150 @@ searchBar.addEventListener('blur', () => {
     }, 300);
 });  
 
-// This function takes the value from searchbar and then save it for temporary as session storage is used.
-function searchBox(){
-    divInSearchBar = document.createElement('div')
-    Line = document.createElement('hr')
-    Line.classList.add("mb-3");
-    divInSearchBar.innerHTML = searchBar.value.charAt(0).toUpperCase() + searchBar.value.slice(1)
-    suggestions.appendChild(divInSearchBar)
-    suggestions.appendChild(Line)
+// Created a async function which takes city name and then feed the UI of the app so we can use it again and again instead of writing it many times
+async function feedUI(citie){
+
+    // Initializing a controller for controlling timeout after 5 seconds
+    const controller = new AbortController();
+    // Function to abort the controller as soon as 5 second is completed
+    const timeoutId = setTimeout(() => controller.abort(), 5000);
+
+    try{
+        // to fetch the data by taking the function variable from API
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c3d1cab6249d4373bc3131136240608&q=${citie}&days=5`, {
+            signal: controller.signal, // Pass the AbortController signal
+        });
     
-    sessionStorage.setItem("saveIT", suggestions.innerHTML);
+        // clear the timeout if the data has return to be on safer side
+        clearTimeout(timeoutId);
+    
+        // Check if the response is ok
+        if (!response.ok) {
+            // if the response is not okay then throw error
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        } else{ // if the response is okay then make the searchbar empty
+            searchBar.value = ''
+        }
+
+        // Convert the returned data to json format
+        const data = await response.json()
+        
+        // Now fill the data on to screen
+        now.innerHTML = `${data.location.localtime}` // Local date and time
+        degreeValue.innerHTML = `${Math.trunc(data.current.temp_c)}&degc` // degree celcius
+        cityLocattion.innerHTML = `${data.location.name.charAt(0).toUpperCase() + data.location.name.slice(1)}` //cityname 
+
+        sessionStorage.setItem('savedCity', cityLocattion.innerText) // created this session storage
+
+        dateAndTime.innerHTML = `(${data.location.region}, ${data.location.country})` //Country name
+        imageWeather.src = `${data.current.condition.icon}` //icon to be displayed
+        preciToday.innerHTML = `${data.current.precip_mm}%` //precipitation
+        humiToday.innerHTML = `${data.current.humidity}%` //humidity
+        windToday.innerHTML = `${data.current.wind_kph}kph` // wind speed
+        valueText.innerHTML = `${data.current.condition.text}` // condition text
+
+        // Updating 1st day forecasted weather data
+        dateOne.innerHTML = data.forecast.forecastday[0].date // local date + 1 day
+        imageWeatherOne.src = data.forecast.forecastday[0].day.condition.icon // icon to be displayed
+        tempTodayOne.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[0].day.avgtemp_c)}&degc` //degree
+        windTodayOne.innerHTML = `Wind - ${data.forecast.forecastday[0].day.maxwind_kph} kph` //wind
+        humiTodayOne.innerHTML = `Humi - ${data.forecast.forecastday[0].day.avghumidity}%` //humidity
+        
+        // Updating 2nd day forecasted weather data
+        dateTwo.innerHTML = data.forecast.forecastday[1].date// local date + 1 day
+        imageWeatherTwo.src = data.forecast.forecastday[1].day.condition.icon // icon to be displayed
+        tempTodayTwo.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[1].day.avgtemp_c)}&degc` //degree
+        windTodayTwo.innerHTML = `Wind - ${data.forecast.forecastday[1].day.maxwind_kph} kph`//wind
+        humiTodayTwo.innerHTML = `Humi - ${data.forecast.forecastday[1].day.avghumidity}%` //humidity
+
+        // Updating 3rd day forecasted weather data
+        dateThree.innerHTML = data.forecast.forecastday[2].date// local date + 1 day
+        imageWeatherThree.src = data.forecast.forecastday[2].day.condition.icon // icon to be displayed
+        tempTodayThree.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[2].day.avgtemp_c)}&degc` //degree
+        windTodayThree.innerHTML = `Wind - ${data.forecast.forecastday[2].day.maxwind_kph} kph`//wind
+        humiTodayThree.innerHTML = `Humi - ${data.forecast.forecastday[2].day.avghumidity}%` //humidity
+
+        // Updating 4th day forecasted weather data
+        dateFour.innerHTML = data.forecast.forecastday[3].date// local date + 1 day
+        imageWeatherFour.src = data.forecast.forecastday[3].day.condition.icon // icon to be displayed
+        tempTodayFour.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[3].day.avgtemp_c)}&degc` //degree
+        windTodayFour.innerHTML = `Wind - ${data.forecast.forecastday[3].day.maxwind_kph} kph`//wind
+        humiTodayFour.innerHTML = `Humi - ${data.forecast.forecastday[3].day.avghumidity}%` //humidity
+
+        // Updating 5th day forecasted weather data
+        dateFive.innerHTML = data.forecast.forecastday[4].date// local date + 1 day
+        imageWeatherFive.src = data.forecast.forecastday[4].day.condition.icon // icon to be displayed
+        tempTodayFive.innerHTML = `Temp - ${Math.trunc(data.forecast.forecastday[4].day.avgtemp_c)}&degc` //degree
+        windTodayFive.innerHTML = `Wind - ${data.forecast.forecastday[4].day.maxwind_kph} kph`//wind
+        humiTodayFive.innerHTML = `Humi - ${data.forecast.forecastday[4].day.avghumidity}%` //humidity
+
+        
+    } catch (error) { // If everything doesn't turns out good this is what should be run
+        // Handle fetch-related errors and timeouts
+        if (error.name === "AbortError") {
+            alert("Request timed out. Please try again.") // If abort error happens this means it's been more than 5s of searching, thus displaying this error
+        } else if (error.message.startsWith("HTTP error!")) {
+            alert(`No Such city as "${citie}", Enter Correct name of city `) // If incorrect city name entered, display this error
+        } else {
+            alert(`Network error: ${error.message}`) // If network issue or internet issue, display this error
+        }
+    }
 }
 
+// This function takes the value from searchbar and then save it for temporary as session storage is used.
+function searchBox(){
+    // Included regex to check if the inserted value is a string or not
+    if (regex.test(searchBar.value) === false) {
+        // if it is false, then alert the user this
+        alert("City name should only contain alphabets and it can't be blank. eg: New york")
+    } else {
+        // if it is true,then save the value in the search bar and return the value
+        divInSearchBar = document.createElement('div') // creating the necessary div
+        Line = document.createElement('hr') // creating the hr line
+        Line.classList.add("mb-3"); // adding the class 
+        divInSearchBar.innerHTML = searchBar.value.charAt(0).toUpperCase() + searchBar.value.slice(1)
+        suggestions.appendChild(divInSearchBar)
+        suggestions.appendChild(Line)
+        sessionStorage.setItem("saveIT", suggestions.innerHTML); // initializing the session storage so that searched value should get saved for a single session
+        return searchBar.value // Most important return the searched city if it is a string
+    }
 
+}
+
+//==================================================================================
+// This function makes sure the value clicked on the searched cities list should update the weather data and feed the UI accordingly
 async function valueFromSearchBar(e){
     cityFromSearchBar = e.target.innerText
+    
+    // create a condition to check if the 'Use current Location' is clicked then return the longitude and latitude
     if (cityFromSearchBar === 'Use current Location') {
         navigator.geolocation.getCurrentPosition((position) => {
             const { latitude, longitude } = position.coords;
             cityInLongLang = `${latitude}, ${longitude}`
             feedUI(cityInLongLang)
         });
-    } else {
+    } else { 
+        // If something else gets clicked then update the weather data and feed the UI accordingly
         feedUI(cityFromSearchBar)
     }
 }
 
+// Calling the above function as soon as the recently searched city tiles is clicked
 suggestions.addEventListener('click', valueFromSearchBar)
 
+//==================================================================================
+// This function makes sure the value entered on the search bar should update the weather data and feed the UI accordingly as soon it is search button is clicked
 async function search(){
 
-    searchBox()
-
-    if (searchBar.value === ""){
-        alert("Please enter city name, City name can't be blank")
-    } else {
-        cityName = searchBar.value
-        
-        feedUI(cityName)
+    // creating a variable to store the value from returned from searchBox() function
+    const names  = searchBox()
+    // if names has some value then it'll get called other wise nothing will happen, to make sure 'undefined' doesn't get triggered
+    if (names) {
+        feedUI(names)
     }
-    
-
 }
 
+// Calling the above function as soon as the search button is clicked
 btn.addEventListener('click', search)
 
-function restoreElement(){
-    savedItem = sessionStorage.getItem('saveIT')
-    console.log('reloaded')
-    console.log(savedItem);
-
-    if (savedItem === null) {
-        suggestions.innerHTML = onlyForUseCurrent.innerHTML
-    } else {
-        suggestions.innerHTML = savedItem
-    }
-}
-
-window.onload = restoreElement
+//=================================================================================
